c535fc254ee12b3dc57a89d0193c48a4f1ea576a
qbao74 <quocbao07042002@gmail.com>
2026-09-23 11:58:29 +0700

    db applications

--- database/migrations/2026_08_26_030741_applications.php
+++ database/migrations/2026_08_26_030741_applications.php
@@ -1,24 +1,22 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        //
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('job_post_id')->constrained()->cascadeOnDelete();
+            $table->string('status')->default('pending');
+            $table->timestamps();
+            $table->unique(['student_id', 'job_post_id']);
+        });
     }
-
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('applications');
     }
 };
